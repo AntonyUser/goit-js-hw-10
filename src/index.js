fa6af14c2b93b7232df3659f6a3198fed8e52bd4
AntonyUser <101988185+AntonyUser@users.noreply.github.com>
2022-09-17 12:09:34 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 import './css/styles.css';
+import { fetchCountries } from './js/fetchCountries';
+import { debounce } from 'lodash.debounce';
+import countryCard from './templates/country-card.hbs';
+
 
 const DEBOUNCE_DELAY = 300;
+
+const inputVal = document.querySelector('#search-box');
+const countryInfoEl = document.querySelector('.country-info')
+inputVal.addEventListener('input', onInputVal);
+
+function onInputVal(event) {
+  const val = event.currentTarget.value;
+  fetchCountries(val)
+  .then((data) => {
+        const country = countryCard(data)
+      countryInfoEl.insertAdjacentHTML('beforeend', country)
+
+  })    
+    }
+
+
+    // ({
+    //     flag: { svg },
+    //     name: { official },
+    //     capital,
+    //     population,
+    //     languages,
+    //   })
---
+++ src/js/fetchCountries.js
@@ -0,0 +1,10 @@
+function fetchCountries(name) {
+  return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,flag,population,languages`).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
+}
+
+export { fetchCountries };
