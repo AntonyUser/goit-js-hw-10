73be0db07adb75b8bcfb7daa4fffab76cab48dc2
AntonyUser <101988185+AntonyUser@users.noreply.github.com>
2022-09-19 13:02:24 +0300

    update h/w

--- src/index.js
+++ src/index.js
@@ -1,30 +1,76 @@
 import './css/styles.css';
 import { fetchCountries } from './js/fetchCountries';
-import { debounce } from 'lodash.debounce';
-import countryCard from './templates/country-card.hbs';
-
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix'
 
 const DEBOUNCE_DELAY = 300;
 
 const inputVal = document.querySelector('#search-box');
-const countryInfoEl = document.querySelector('.country-info')
-inputVal.addEventListener('input', onInputVal);
+const countryInfoEl = document.querySelector('.country-info');
+const countryList = document.querySelector('.country-list');
 
-function onInputVal(event) {
-  const val = event.currentTarget.value;
-  fetchCountries(val)
-  .then((data) => {
-        const country = countryCard(data)
-      countryInfoEl.insertAdjacentHTML('beforeend', country)
+inputVal.addEventListener('input', debounce(onInputVal, DEBOUNCE_DELAY));
 
-  })    
+function onInputVal() {
+  countryList.innerHTML = '';
+  countryInfoEl.innerHTML = '';
+  const trimVal = inputVal.value.trim()
+  if (trimVal) {
+    fetchCountries(trimVal).then(onRenderCountry).catch(onCatchError);
+  }
+  
+
+function onRenderCountry(data) {
+  if (data.length > 10) {
+    return Notiflix.Notify.warning('Too many matches found. Please enter a more specific name.');
+  }
+    data.forEach(country => {
+     const { flags: { svg },
+      name: { official },
+      capital,
+      population,
+      languages,
+    } = country
+    if (data.length === 1) {
+      const dataEl = `<img class="country-icon" src='${svg}' alt='flag' width='60' />
+      <h2 class='county-info_name'>${official}</h2>
+      <p class='country-info_description'>
+        Capital:
+        <span class='country-info_val'>${capital}</span>
+      </p>
+      <p class='country-info_description'>
+        Population:
+        <span class='country-info_val'>${population}</span>
+      </p>
+      <p class='country-info_description'>
+        Language:
+        <span class='country-info_val'>${Object.values(languages).join(
+          ', '
+        )}</span>
+      </p>`;
+      countryInfoEl.innerHTML = dataEl;
+      }
+      
+    else {
+      const dataEl = `<li>
+      <img class="country-icon" src="${svg}" width='20' height='15'></img>
+      <p class="country-list__text">${official}</p>
+    </li>`;
+    countryList.insertAdjacentHTML('beforeend', dataEl);
     }
+  })
+
+}}
+
+function onCatchError() {
+  Notiflix.Notify.failure('Oops, there is no country with that name')
+}
 
 
-    // ({
-    //     flag: { svg },
-    //     name: { official },
-    //     capital,
-    //     population,
-    //     languages,
-    //   })
+Notiflix.Notify.init({
+
+  warning: {
+    background: '#30d5c8'
+  }
+
+});
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,5 +1,6 @@
 function fetchCountries(name) {
-  return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,flag,population,languages`).then(response => {
+  return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,flags,population,languages`)
+  .then(response => {
     if (!response.ok) {
       throw new Error(response.status);
     }
